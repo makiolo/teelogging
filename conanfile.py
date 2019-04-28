e03504ce7eac5bf1c688bf9ffaff8617ff6a3e95
Ricardo Marmolejo García <makiolo@gmail.com>
2019-04-28 15:24:41 +0200

    .

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class NpmMasMas(ConanFile):
     def requirements(self):
         self.requires('gtest/1.8.1@bincrafters/stable')
         self.requires('spdlog/1.3.1@bincrafters/stable')
-        self.requires('fmt/4.1.0@bincrafters/stable')
+        self.requires('fmt/5.3.0@bincrafters/stable')
 
     def source(self):
         self.run("git clone {}".format(self.url))
